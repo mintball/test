2776330753ea6b7af27b8c35d030831a3adf6005
mintball <bolza25120@gmail.com>
2022-01-06 04:21:15 +0000

    test file

---
+++ week6.py
@@ -0,0 +1,21 @@
+#program calculator so function + - * /
+
+def add(a,b):
+    print("add: ",a+b)
+
+def sub(a,b):
+    print("sub: ",a-b)
+
+def mul(a,b):
+    print("mul: ",a*b)
+
+def div(a,b):
+    print("div: ",a/b)
+
+a = int(input("A = "))
+b = int(input("B = "))
+
+add(a,b)
+sub(a,b)
+mul(a,b)
+div(a,b)
